f46ad92081608907e65a3772db1c42f076681149
moorthi <velayuthamoorthi.l@guruits.com>
2025-03-05 14:57:56 +0530

    initial commit

---
+++ DatastructureandAlgorithms/src/com/moorthi/JavaProgram.java
@@ -0,0 +1,8 @@
+package com.moorthi;
+
+public class JavaProgram {
+
+public static void main(String[] args) {
+	System.out.println("This one is for check the Git Hub Repo!\nLet's Go have great journey started from here.");
+}
+}
